36d1e7a2f1d5f0af395e89ed27decf3355ee64bd
Adequm <adequm@bk.ru>
2022-02-21 00:15:27 +0300

    add dist/

---
+++ dist/precache-manifest.b886fafe68377de8341c4b5edcf28b0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2724c685bc30d050de54",
+    "url": "/calculator/css/app.3ae4def2.css"
+  },
+  {
+    "revision": "8bf209bf8ac81043e662f44c3cd28171",
+    "url": "/calculator/feather-sprite.svg"
+  },
+  {
+    "revision": "14a9dfc6e87ab2fef46516e4f8aa058e",
+    "url": "/calculator/img/confetti.14a9dfc6.svg"
+  },
+  {
+    "revision": "bef9fcaee808f70bc94e1b1eb958b3f3",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2724c685bc30d050de54",
+    "url": "/calculator/js/app.e3608b23.js"
+  },
+  {
+    "revision": "cf14d5a39de41fd8e39e",
+    "url": "/calculator/js/chunk-vendors.688141c1.js"
+  },
+  {
+    "revision": "d760802630e614fc481195e647cad03c",
+    "url": "/calculator/manifest.json"
+  }
+]);
